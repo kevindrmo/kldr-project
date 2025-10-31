f63591536c62d607072f56597f858435e5e2600a
Kevin Lorenzo <kevinlorenzo.diraimo@uzh.ch>
2025-10-31 13:11:46 +0100

    added some comments about app structure in app.py

--- app/app.py
+++ app/app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path
 
+#app is only meant to define navigation between pages
+# all work is happening within pages SEPARATELY
+
 st.set_page_config("kldr-final-project", page_icon = "📊", layout = "wide")
 
 # within the app ONLY define my pages!
