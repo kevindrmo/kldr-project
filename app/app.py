2230a6696ce725dbff1beae22f0db151b59c20dd
Kevin Lorenzo <kevinlorenzo.diraimo@uzh.ch>
2025-10-31 12:07:11 +0100

    add first content

--- app/app.py
+++ app/app.py
@@ -12,3 +12,19 @@ ROOT = Path(__file__).parent
 df_disney = pd.read_csv(ROOT/ "data"/ "disney_movies_clean.csv")
 
 st.dataframe(df_disney)
+
+# how to organize my code
+
+st.header("Project Intro")
+
+st.header("Data overview")
+
+col_data, _, col_chart = st.columns((0.8, 0.005, 1))
+
+with col_data:
+    st.subheader("Raw Data")
+    st.dataframe(df_disney)
+
+with col_chart:
+    st.subheader("Data Overview")
+    st.markdown("Placeholder for chart")
