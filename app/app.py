6001faab6a0a992b5d6f5fc05be6fc856cb02989
Kevin Lorenzo <kevinlorenzo.diraimo@uzh.ch>
2025-10-31 11:54:34 +0100

    add dataand display it

--- app/app.py
+++ app/app.py
@@ -1,7 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Sample Project")
 
 st.header("Hello world!")
 
 st.markdown("Some additional text")
+
+
+df_disney = pd.read_csv("data/disney_movies_clean.csv")
+
+st.dataframe(df_disney)
