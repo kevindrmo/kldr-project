7e0a157dc0a6acbe2b1f6d9b9cdd0348b7470aad
Kevin Lorenzo <kevinlorenzo.diraimo@uzh.ch>
2025-10-31 11:56:48 +0100

    fix path

--- app/app.py
+++ app/app.py
@@ -8,6 +8,6 @@ st.header("Hello world!")
 st.markdown("Some additional text")
 
 
-df_disney = pd.read_csv("data/disney_movies_clean.csv")
+df_disney = pd.read_csv("app/data/disney_movies_clean.csv")
 
 st.dataframe(df_disney)
